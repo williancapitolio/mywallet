ffcc9018b8637cf008aa15d5e3a8cf7100445ee2
Willian <williansilvap25@gmail.com>
2023-11-16 11:52:09 -0300

    add a wrapper provider to wrap external providers

--- src/main.tsx
+++ src/main.tsx
@@ -2,26 +2,14 @@ import React from "react";
 
 import ReactDOM from "react-dom/client";
 
-import { ThemeProvider } from "styled-components";
-
-import Dark from "./styles/themes/Dark";
-/* import Light from "./styles/themes/Light"; */
-
-import { GlobalStyles } from "./styles/Global";
-
-import { Provider } from "react-redux";
-
-import { store } from "./store";
+import { WrapperProvider } from "./providers/WrapperProvider";
 
 import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={Dark}>
-      <GlobalStyles />
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ThemeProvider>
+    <WrapperProvider>
+      <App />
+    </WrapperProvider>
   </React.StrictMode>
 );
---
+++ src/providers/WrapperProvider.tsx
@@ -0,0 +1,27 @@
+import { Provider } from "react-redux";
+
+import { store } from "../store";
+
+import { ThemeProvider } from "styled-components";
+
+import Dark from "../styles/themes/Dark";
+/* import Light from "../styles/themes/Light"; */
+
+import { GlobalStyles } from "../styles/Global";
+
+type WrapperProviderProps = {
+  children: React.ReactNode;
+};
+
+export const WrapperProvider = ({ children }: WrapperProviderProps) => {
+  return (
+    <>
+      <Provider store={store}>
+        <ThemeProvider theme={Dark}>
+          <GlobalStyles />
+          {children}
+        </ThemeProvider>
+      </Provider>
+    </>
+  );
+};
