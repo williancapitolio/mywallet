93b739470082433bee4bd8ccc5a5f419a887ded5
Willian <williansilvap25@gmail.com>
2023-11-23 00:07:50 -0300

    update layout and structure

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,11 +1,11 @@
-import { Home } from "../Home";
+import { Structure } from "../Structure";
 
 import { FormUser } from "../../components/forms/FormUser";
 
 export const Layout = () => {
   return (
     <>
-      <Home />
+      <Structure />
       <FormUser />
     </>
   );
